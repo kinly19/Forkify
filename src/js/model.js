bef717bcdd6da88443e9559dee7025af012bea4c
unknown <kinchoily@hotmail.com>
2022-10-10 16:37:32 +0100

    model.js - Add 'loadSearchResults' logic for handling search query results

--- src/js/model.js
+++ src/js/model.js
@@ -4,12 +4,16 @@ import { getJSON } from './helpers';
 // Model
 export const state = {
   recipe: {},
+  recipes: {
+    query: "",
+    results: [],
+  },
 }; 
 
 // Fetch and storing data into state
 export const loadRecipe = async (id) => {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
     // Format object
     const { recipe } = data.data;
     state.recipe = {
@@ -26,6 +30,25 @@ export const loadRecipe = async (id) => {
 
   } catch (err) {
     console.error(err);
+    throw err;
+  }
+};
+
+export const loadSearchResults = async query => {
+  try {
+    const searchRes = await getJSON(`${API_URL}?search=${query}`);
+    const {recipes} = searchRes.data;
+    // Map through array
+    state.recipes.results = recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        imageURL: rec.image_url,
+      };
+    });
+  } catch (err) {
+    console.log(err);
     throw err;
   }
 };

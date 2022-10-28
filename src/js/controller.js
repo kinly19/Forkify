88410a8b46617ba770adacc367dfc1cc75c1c577
unknown <kinchoily@hotmail.com>
2022-10-28 20:29:15 +0100

    controller.js - Add 'controlAddRecipe' controller for handling new recipes

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,11 @@
 import * as model from './model.js';
+import { MODAL_CLOSE_SEC } from './config.js'
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 // Support old browser
 import 'core-js/stable'; // polyfil
 import 'regenerator-runtime/runtime'; // polyfil async functions
@@ -86,8 +88,40 @@ const controlBookmarks = () => {
   bookmarksView.render(model.state.bookmarks);
 }
 
+const controlAddRecipe = async (newRecipe) => {
+  try {
+    // Show spinner
+    addRecipeView.renderSpinner()
+    // Upload recipe
+    await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe);
+
+    // render recipe view
+    recipeView.render(model.state.recipe)
+    
+    // Show success message
+    addRecipeView.renderMessage();
+
+    // Render bookmarks view
+    bookmarksView.render(model.state.bookmarks);
+
+    // Change ID in url
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
+    // Close modal after a few seconds
+    setTimeout(() => {
+      addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SEC * 1000);
+    
+  } catch (err) {
+    addRecipeView.renderError(err.message);
+  }
+ 
+}
+
 const init = () => {
   bookmarksView.addhandlerRender(controlBookmarks);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
   recipeView.addHandlerAddBookmark(controlAddBookmark);

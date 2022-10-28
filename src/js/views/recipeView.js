cc35660b6cde6289f3bce91db5209c8d8ca41cad
unknown <kinchoily@hotmail.com>
2022-10-28 20:38:30 +0100

    recipeView.js - Update view for conditionally applying class attribute

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -74,7 +74,7 @@ class RecipeView extends View {
         </div>
       </div>
 
-      <div class="recipe__user-generated">
+      <div class="recipe__user-generated ${this._data.key ? "" : "hidden"}">
         <svg>
           <use href="${icons}.svg#icon-user"></use>
         </svg>

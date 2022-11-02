94bbfc3709893dbb25e5e2d8d078e4f65069c7d6
unknown <kinchoily@hotmail.com>
2022-11-02 12:18:36 +0000

    config.js - Update config file

--- src/js/config.js
+++ src/js/config.js
@@ -2,4 +2,5 @@ export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const API_KEY = "c3d41371-6301-4950-8214-5802463d2010";
 export const TIMEOUT_SEC = 10;
 export const RESULTS_PER_PAGE = 10;
-export const MODAL_CLOSE_SEC = 2.5;
+export const MODAL_CLOSE_SEC = 2;
+export const RENDER_MARKUP_SEC = 0.5;
